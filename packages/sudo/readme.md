1cd2f3e8192140491d7a481604ce9e289a317fc7
Shigma <shigma10826@gmail.com>
2023-06-28 18:57:46 +0800

    feat(sudo): add sudo plugin

---
+++ packages/sudo/readme.md
@@ -0,0 +1,9 @@
+# [koishi-plugin-sudo](https://common.koishi.chat/plugins/sudo.html)
+ 
+[![npm](https://img.shields.io/npm/v/koishi-plugin-sudo?style=flat-square)](https://www.npmjs.com/package/koishi-plugin-sudo)
+
+Simulate other contexts. | 模拟其他上下文中的指令调用。
+
+## 文档
+
+<https://common.koishi.chat/plugins/sudo.html>
---
+++ packages/sudo/src/index.ts
@@ -0,0 +1,73 @@
+import { Context, Schema, Session } from 'koishi'
+
+export interface Config {}
+
+export const name = 'sudo'
+export const using = ['database'] as const
+export const Config: Schema<Config> = Schema.object({})
+
+function parsePlatform(target: string) {
+  const index = target.indexOf(':')
+  const platform = target.slice(0, index)
+  const id = target.slice(index + 1)
+  return [platform, id]
+}
+
+export function apply(ctx: Context) {
+  ctx.i18n.define('zh', require('./locales/zh-CN'))
+
+  ctx.command('sudo <command:text>', { authority: 3 })
+    .userFields(['authority'])
+    .option('user', '-u [id:user]')
+    .option('member', '-m [id:user]')
+    .option('channel', '-c [id:channel]')
+    .action(async ({ session, options }, message) => {
+      if (!message) return session.text('.expect-command')
+
+      if (options.member) {
+        if (session.subtype === 'private') {
+          return session.text('.invalid-private-member')
+        }
+        options.channel = session.cid
+        options.user = options.member
+      }
+
+      if (!options.user && !options.channel) {
+        return session.text('.expect-context')
+      }
+
+      const sess = new Session(session.bot, session)
+      sess.send = session.send.bind(session)
+      sess.sendQueued = session.sendQueued.bind(session)
+
+      if (!options.channel) {
+        sess.subtype = 'private'
+      } else if (options.channel !== session.cid) {
+        sess.channelId = parsePlatform(options.channel)[1]
+        sess.subtype = 'group'
+        await sess.observeChannel()
+      } else {
+        sess.channel = session.channel
+      }
+
+      if (options.user && options.user !== session.uid) {
+        sess.userId = sess.author.userId = parsePlatform(options.user)[1]
+        const user = await sess.observeUser(['authority'])
+        if (session.user.authority <= user.authority) {
+          return session.text('internal.low-authority')
+        }
+      } else {
+        sess.user = session.user
+      }
+
+      if (options.member) {
+        const info = await session.bot.getGuildMember?.(sess.guildId, sess.userId).catch(() => ({}))
+        Object.assign(sess.author, info)
+      } else if (options.user) {
+        const info = await session.bot.getUser?.(sess.userId).catch(() => ({}))
+        Object.assign(sess.author, info)
+      }
+
+      await sess.execute(message)
+    })
+}
