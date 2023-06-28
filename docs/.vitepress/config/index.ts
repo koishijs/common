b5d00abc971aa5233898c860b0906883b0297e56
Shigma <shigma10826@gmail.com>
2023-06-28 18:29:52 +0800

    docs: setup docs

---
+++ docs/.vitepress/config/index.ts
@@ -0,0 +1,22 @@
+import { defineConfig } from '@koishijs/vitepress'
+
+const isDev = process.env.NODE_ENV === 'development' || process.env.VERCEL_ENV === 'preview'
+
+export default defineConfig({
+  title: '@koishijs/common',
+
+  locales: {
+    'zh-CN': require('./zh-CN'),
+    ...(isDev ? {
+    } : {}),
+  },
+
+  themeConfig: {
+    indexName: 'common',
+    logo: 'https://koishi.chat/logo.png',
+
+    socialLinks: {
+      github: 'https://github.com/koishijs/common',
+    },
+  },
+})
