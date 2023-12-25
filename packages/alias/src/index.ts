41ce96105ce029988c7188f702134783222a3e40
Shigma <shigma10826@gmail.com>
2023-12-26 04:07:33 +0800

    feat(alias): support config-based persistence, fix #7

--- packages/alias/src/index.ts
+++ packages/alias/src/index.ts
@@ -1,33 +1,47 @@
-import { Command, Context, Dict, Schema } from 'koishi'
+import { Context, Dict, Schema } from 'koishi'
 import {} from '@koishijs/plugin-help'
 
-export interface Config {}
+export interface Config {
+  aliases?: Dict<string>
+}
 
-export const Config: Schema<Config> = Schema.object({})
+export const Config: Schema<Config> = Schema.object({
+  aliases: Schema.dict(String).hidden(),
+})
 
 export const name = 'alias'
 
 export function apply(ctx: Context, config: Config) {
   ctx.i18n.define('zh-CN', require('./locales/zh-CN'))
 
-  const aliases: Dict<Command> = Object.create(null)
+  function createAlias(name: string, command: string) {
+    return ctx.command(name, { hidden: true })
+      .action(async ({ session, source }) => {
+        await session.execute(command + source.slice(name.length))
+      })
+  }
+
+  for (const name in config.aliases) {
+    createAlias(name, config.aliases[name])
+  }
 
   ctx.command('alias <name:string> <command:text>', { authority: 3, checkArgCount: true })
     .action(async ({ session }, name, command) => {
-      const old = aliases[name]
-      if (old) old.dispose()
-      aliases[name] = ctx.command(name, { hidden: true })
-        .action(async (argv) => {
-          await session.execute(command + argv.source.slice(name.length))
-        })
+      const old = ctx.$commander.get(name)
+      old?.dispose()
+      config.aliases[name] = command
+      createAlias(name, command)
+      ctx.scope.update(config, false)
       return session.text(old ? '.updated' : '.created', [name])
     })
 
   ctx.command('unalias <name:string>', { authority: 3, checkArgCount: true })
     .action(async ({ session }, name) => {
-      if (!aliases[name]) return session.i18n('.not-found', [name])
-      aliases[name].dispose()
-      delete aliases[name]
+      const old = ctx.$commander.get(name)
+      if (!old) return session.i18n('.not-found', [name])
+      old.dispose()
+      delete config.aliases[name]
+      ctx.scope.update(config, false)
       return session.i18n('.deleted', [name])
     })
 }
--- packages/alias/tests/index.spec.ts
+++ packages/alias/tests/index.spec.ts
@@ -18,7 +18,8 @@ describe('koishi-plugin-alias', () => {
   it('basic support', async () => {
     await client.shouldReply('alias foo echo 111', '已成功创建别名指令 foo。')
     await client.shouldReply('foo 222', '111 222')
-    await client.shouldReply('alias foo echo 111', '已成功更新别名指令 foo。')
+    await client.shouldReply('alias foo echo 222', '已成功更新别名指令 foo。')
+    await client.shouldReply('foo 111', '222 111')
 
     await client.shouldReply('unalias foo', '已成功删除别名指令 foo。')
     await client.shouldNotReply('foo 222')
